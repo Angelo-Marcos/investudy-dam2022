0e16ccaac3eca8518f70ffcb12e9925504d40066
unknown <angelopc7@gmail.com>
2022-03-27 12:19:52 -0300

    Criação do tema com cores

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+    colors: {
+        primary: '#61DD6D',
+        secondary: '#A1DDA7',
+
+        shape: '#000000',
+        title: '#0CA012',
+
+        background: '#E5E5E5',
+        cardView: '#FFFFFF',
+
+        income: '#3F51B5',
+        expense: '#CA0606',
+    }
+}
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,10 +1,9 @@
-import { StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
 
-export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: ${(props) => props.theme.colors.background};
+    align-items: center;
+    justify-content: center;
+`;
